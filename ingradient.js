cd2e8927c2d3143810f5f911c0770c2103730bbf
tammana1 <82446177+tammana1@users.noreply.github.com>
2021-06-15 12:27:13 +0530

    Add files via upload

---
+++ ingradient.js
@@ -0,0 +1,2 @@
+Tea  
+1/2 milk , 1/2 Water , some tea leaves, 1 spone sugar .
---
+++ recipe.js
@@ -0,0 +1,6 @@
+1st - take a bowl of water and keep it for boiling 
+2nd - add milk , water
+3rd - boil for 2 min 
+4th - add tea leaves, sugar 
+5th- boil for 1 min 
+6th- tea is ready .
